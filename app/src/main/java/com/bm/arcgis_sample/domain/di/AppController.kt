ee560af6e04e8a8c8d504327e04fc3a435794f88
Omkar Sawant <omkar.s@neosoftmail.com>
2025-03-23 13:39:57 +0530

    - Saved location markers added on map

--- app/src/main/java/com/bm/arcgis_sample/domain/di/AppController.kt
+++ app/src/main/java/com/bm/arcgis_sample/domain/di/AppController.kt
@@ -1,6 +1,10 @@
 package com.bm.arcgis_sample.domain.di
 
 import android.app.Application
+import android.graphics.drawable.BitmapDrawable
+import androidx.core.content.ContextCompat
+import com.arcgismaps.mapping.symbology.PictureMarkerSymbol
+import com.bm.arcgis_sample.R
 import dagger.hilt.android.HiltAndroidApp
 
 
@@ -10,8 +14,32 @@ class AppController : Application() {
         lateinit var instance: AppController
     }
 
+    fun createPinSymbol(): PictureMarkerSymbol {
+        //add a graphic for the tapped point
+        val pinDrawable = ContextCompat.getDrawable(
+            instance.applicationContext,
+            R.drawable.favourites
+        )
+        //add a graphic for the tapped point
+        val pinSymbol = PictureMarkerSymbol.createWithImage(
+            pinDrawable as BitmapDrawable
+        )
+        pinSymbol.apply {
+            // resize the dimensions of the symbol
+            width = 50f
+            height = 50f
+            // the image is a pin so offset the image so that the pinpoint
+            // is on the point rather than the image's true center
+            leaderOffsetX = 30f
+            offsetY = 25f
+        }
+        return pinSymbol
+    }
+
     override fun onCreate() {
         super.onCreate()
         instance = this
     }
+
+
 }
--- app/src/main/java/com/bm/arcgis_sample/presentation/map_view/view_model/MapViewModel.kt
+++ app/src/main/java/com/bm/arcgis_sample/presentation/map_view/view_model/MapViewModel.kt
@@ -1,13 +1,22 @@
 package com.bm.arcgis_sample.presentation.map_view.view_model
 
+import android.content.Context
+import android.graphics.drawable.BitmapDrawable
 import android.widget.Toast
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddLocation
+import androidx.compose.material.icons.twotone.Pin
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asFlow
 import androidx.lifecycle.viewModelScope
 import com.arcgismaps.geometry.GeometryEngine
 import com.arcgismaps.geometry.Point
 import com.arcgismaps.geometry.SpatialReference
 import com.arcgismaps.location.LocationDisplayAutoPanMode
 import com.arcgismaps.mapping.Viewpoint
+import com.arcgismaps.mapping.symbology.PictureMarkerSymbol
+import com.arcgismaps.mapping.view.Graphic
 import com.arcgismaps.mapping.view.GraphicsOverlay
 import com.arcgismaps.mapping.view.LocationDisplay
 import com.arcgismaps.mapping.view.SingleTapConfirmedEvent
@@ -15,18 +24,23 @@ import com.arcgismaps.toolkit.geoviewcompose.MapViewProxy
 import com.bm.arcgis_sample.data.repository.feature_geocode.RemoteDataSourceGeocode
 import com.bm.arcgis_sample.data.repository.feature_saved_address.LocalDataSourceSavedAddress
 import com.bm.arcgis_sample.data.repository.model.DtoReverseGeocodeResponse
+import com.bm.arcgis_sample.domain.di.AppController
 import com.bm.arcgis_sample.domain.utill.ScreenState
 import com.bm.arcgis_sample.presentation.map_view.state.UiStateMapScreen
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -42,14 +56,13 @@ class MapViewModel @Inject constructor(
     val uiState = _uiState.asStateFlow()
     var addressResponse : DtoReverseGeocodeResponse? = null
 
-    private val _showMessage : MutableSharedFlow<String> = MutableSharedFlow()
+    private val _showMessage : MutableSharedFlow<String> = MutableSharedFlow(1, 1, BufferOverflow.DROP_LATEST)
     val showMessage = _showMessage.asSharedFlow()
 
     val mapViewProxy = MapViewProxy()
 
-    val graphicsOverlay = GraphicsOverlay().apply {
-        this.isPopupEnabled = true
-    }
+    val graphicsOverlay = GraphicsOverlay()
+
     val locationDisplay = LocationDisplay().apply {
         showLocation = true
         showPingAnimationSymbol = true
@@ -58,20 +71,32 @@ class MapViewModel @Inject constructor(
     }
 
     init {
-        observeCurrentLocation()
+        showMarkers()
     }
 
-    private fun observeCurrentLocation(){
+    private val pinSymbol by lazy {
+        AppController.instance.createPinSymbol()
+    }
+
+
+    private fun showMarkers(){
         viewModelScope.launch {
-            locationDisplay.location.onEach { lc ->
-                lc?.let { location->
-                    _uiState.update { state ->
-                        state.copy(currentLocation = Viewpoint(location.position))
+            repositoryLocalSavedAddress.getAllAddress()
+                .asFlow()
+                .onEach {
+                    graphicsOverlay.graphics.apply {
+                        clear()
+                        it.forEach {
+                            val point  = GeometryEngine.projectOrNull(Point(it.long.toDouble(), it.lat.toDouble()), SpatialReference.wgs84()) as Point
+                            add(Graphic(point,pinSymbol))
+                        }
                     }
                 }
-            }.stateIn(viewModelScope , started = SharingStarted.WhileSubscribed() , null)
+                .flowOn(Dispatchers.IO)
+                .collect()
         }
     }
+
 
     fun navigateToCurrentLocation() {
         viewModelScope.launch {
